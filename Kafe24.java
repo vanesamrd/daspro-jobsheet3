f769a1182d01a23761064c6e650750f5904444ce
vanesamrd <paneja435@gmail.com>
2024-09-11 10:17:19 +0700

    feat tambahkan hirungan kafe

--- Kafe24.java
+++ Kafe24.java
@@ -1,5 +1,11 @@
 import java.util.Scanner;
+/**
+ * Kafe24
+ */
 public class Kafe24 {
+
+    
+}
     
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
@@ -8,6 +14,7 @@ public class Kafe24 {
         double hargaKopi = 12000.0, hargaTeh = 7000.0, hargaRoti = 20000.0;
         float diskon = 10 /100f;
         double totalHarga, nominalBayar;
+        
         System.out.println("Masukkan keanggotaan (true/false): ");
         keanggotaan = input.nextBoolean();
         System.out.println("Masukkan jumlah pembelian kopi: ");
@@ -16,11 +23,14 @@ public class Kafe24 {
         jmlTeh = input.nextInt();
         System.out.println("Masukkan jumlah pembelian roti: ");
         jmlRoti = input.nextInt();
+        
         totalHarga = (jmlKopi * hargaKopi) + (jmlTeh * hargaTeh) + (jmlRoti * hargaRoti);
+        
         nominalBayar = totalHarga - (diskon * totalHarga);
+        
         System.out.println("Keanggotaan pelanggan " + keanggotaan);
         System.out.println("Item pembelian " + jmlKopi + "Kopi, " + jmlTeh + "Teh, " + jmlRoti + "Roti, ");
         System.out.println("Nominal bayar Rp " + nominalBayar);
 
     }
-}
+
