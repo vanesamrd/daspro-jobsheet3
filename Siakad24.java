5c10aa22df4d72b90b701aa647068e4c7715c767
vanesamrd <paneja435@gmail.com>
2024-09-11 10:02:58 +0700

    feat tambahkan hitungan siakad

--- Siakad24.java
+++ Siakad24.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 
+import java.util.Scanner;
+
 /**
  * Siakad24
  */
@@ -10,27 +12,32 @@ public class Siakad24 {
         String nama, nim;
         char kelas;
         byte absen;
-        double nilaiKuis, nilaiTugas, nilaiUjian, nilaiAkhir;
-        System.out.println("Masukkan nama: ");
+        double nilaiKuis, nilaiTugas, nilaiUjian, nilaiUTS, nilaiUAS, nilaiAkhir;
+        
+        System.out.println("Masukkan nama\t\t: ");
         nama = sc.nextLine();
-        System.out.println("Masukkan NIM: ");
+        System.out.println("Masukkan NIM\t\t: ");
         nim = sc.nextLine();
-        System.out.println("Masukkan kelas: ");
+        System.out.println("Masukkan kelas\t\t: ");
         kelas = sc.nextLine().charAt(0);
-        System.out.println("Masukkan nomor absen: ");
+        System.out.println("Masukkan nomor absen\t: ");
         absen = sc.nextByte();
-        System.out.println("Masukkan nilai kuis: ");
+        System.out.println("Masukkan nilai kuis\t: ");
         nilaiKuis = sc.nextDouble();
-        System.out.println("Masukkan nilai tugas: ");
+        System.out.println("Masukkan nilai tugas\t: ");
         nilaiTugas = sc.nextDouble();
-        System.out.println("Masukkan nilai ujian: ");
+        System.out.println("Masukkan nilai ujian\t: ");
         nilaiUjian = sc.nextDouble();
-        System.out.println("Masukkan nilai akhir: ");
+        System.out.println("Masukkan nilai UTS\t: ");
+        nilaiUTS = sc.nextDouble();
+        System.out.println("Masukkan nilai UAS\t: ");
+        nilaiUAS = sc.nextDouble();
+        System.out.println("Masukkan nilai akhir\t: ");
         nilaiAkhir = sc.nextDouble();
-        nilaiAkhir = (nilaiKuis + nilaiTugas + nilaiUjian) / 3;
-        System.out.println("Nama: " + nama + " NIM: " + nim);
-        System.out.println("Kelas: " + kelas + " Absen: " + absen);
-        System.out.println("Nilai akhir: " + nilaiAkhir);
+       
+        nilaiAkhir = (nilaiKuis / 100 * 20) + (nilaiTugas / 100 * 15) + (nilaiUTS / 100 * 30) + (nilaiUAS / 100 * 35);
+       
+        System.out.printf("Output:\nNAma\t: %s\nNIM\t: %s\nKelas\t: %s\nAbsen\t: %s\nNilai Akhir\t: %s", nama, nim, kelas, absen, nilaiAkhir);
     
 
     }
